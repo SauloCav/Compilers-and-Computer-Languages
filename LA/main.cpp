76075b3bac80ed0163eaf36b679a99b3e536fe52
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-04-23 22:24:13 -0300

    Update main.cpp

--- LA/main.cpp
+++ LA/main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
 
-	char str[200] = "int \n a = b + \n 1c; ximira ! \t == if && false begin end for \n #pla# @ximira ninguem peixola@ 224345 4.5";
+	char str[200] = "";
 
 	parse(str);
 
