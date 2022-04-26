1f9229379e33f15e6cbde05734d8715c766aa88c
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-04-26 08:25:12 -0300

    Update main.cpp

--- LA/main.cpp
+++ LA/main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
 
-	char str[400] = "begin end \n && || ! \n + - * / ** % ++ -- \n < > == != != >= === !== \n = += -= *= /= %= **= <<= >>= >>>= &= ^= |= \n if else while do break continue int double float return char case sizeof long short typedef switch unsigned void static struct goto new operator enum enum enum this protected throw catch class public try for register inline signed default \n 123 542 \n 45.09 87.4 \n #ximira# #peixola# \n false true ";
+	char str[800] = "begin end \n && || ! \n + - * / ** % ++ -- \n < > == != != >= === !== \n = += -= *= /= %= **= <<= >>= >>>= &= ^= |= \n if else while do break continue int double float return char case sizeof long short typedef switch unsigned void static struct goto new operator enum enum enum this protected throw catch class public try for register inline signed default \n 123 542 \n 45.09 87.4 \n #ximira# #peixola# \n false true ";
 
 	parse(str);
 
