6d3587fcda1adec87e8b5e9aab5ece89a24c04c4
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-05-01 19:45:21 -0300

    Delete main.cpp

--- LA/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "lexicalAnalyzer.h"
-
-int main() {
-
-	char str[800] = "begin end \n && || ! \n + - * / ** % ++ -- \n < > == != != >= === !== \n = += -= *= /= %= **= <<= >>= >>>= &= ^= |= \n if else while do break continue int double float return char case sizeof long short typedef switch unsigned void static struct goto new operator enum enum enum this protected throw catch class public try for register inline signed default \n 123 542 \n 45.09 87.4 \n #ximira# #peixola# \n false true @I do not regret anything@ @@  ";
-
-	parse(str);
-
-}
