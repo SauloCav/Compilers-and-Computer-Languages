952a88c2fc4b788680495b8c903487aa1195bc84
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-05-05 18:36:52 -0300

    Add files via upload

---
+++ LA/main.cpp
@@ -0,0 +1,20 @@
+#include "lexicalAnalyzer.h"
+
+int main() {
+
+	char str[LEN_ARRAY] = " int main () \n begin \n int x = 1; \n int y = 3; \n int z = 6; \n @this is a comment@ \n bool a = false; b = true; x < y \n end ";
+	TokenCol* col = createCollection(100);
+	
+	printf("\nInput:\n");
+	for (int i = 0; i < LEN_ARRAY; i ++){
+		printf("%c", str[i]);
+	}
+	
+	if((hasError(str)) == false){
+		tokenizer(str, col);
+	}
+	
+	//printCollection(col);
+
+}
+
