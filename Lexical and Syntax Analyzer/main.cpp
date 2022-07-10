a61cf1c77a75771cf2ac681e288df028d84a2474
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-07-10 10:29:34 -0300

    Add files via upload

---
+++ Lexical and Syntax Analyzer/main.cpp
@@ -0,0 +1,21 @@
+#include "compiler.h"
+
+int main() {
+
+	char str[LEN_ARRAY] = " int main () \n begin \n int x = 1 ; \n int y = 3 ; \n int z = 6 ; \n @this is a comment@ \n bool a = false ; \n bool b = true ; \n true || false ; \n x < y ; \n end ";
+	TokenCol* col = createCollection(100);
+	
+	printf("\nInput:\n");
+	for (int i = 0; i < LEN_ARRAY; i ++){
+		printf("%c", str[i]);
+	}
+	
+	if((hasErrorLex(str)) == false){
+		tokenizer(str, col);
+	}
+	
+	printCollection(col);
+	parser(col);
+
+}
+
